58e00b1bdb408073a8a40ef62f4c5ae0d163770f
Edoardo Tronci <51919409+Edoardo-Tronci@users.noreply.github.com>
2022-01-06 17:02:34 +0100

    Add files via upload

---
+++ Datasets/blogfeed.py
@@ -0,0 +1,20 @@
+from sklearn.preprocessing import StandardScaler
+import numpy as np
+from sklearn.utils import shuffle
+
+# print('************* BLOGDATA ****************')
+
+DATA = np.genfromtxt('blogfeed.csv',delimiter=',')
+Xtr = DATA[:52397,:-1]
+ytr = DATA[:52397,-1]
+
+# print(Xtr.shape," ",ytr.shape)
+# print('number of distinct values in target: ',len(np.unique(ytr)),'\n')
+
+scalerx = StandardScaler()
+scalery = StandardScaler()
+scalerx.fit(Xtr)
+scalery.fit(ytr.reshape(-1,1))
+
+X_train = scalerx.transform(Xtr)
+y_train = scalery.transform(ytr.reshape(-1,1))
